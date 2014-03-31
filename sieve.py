6da397dccb17a1e788799a6cf652ce83ca93c76d
piyushroshan <piyushroshan@gmail.com>
2014-04-01 03:15:56 +0530

    Defined base class sieve

---
+++ sieve.py
@@ -0,0 +1,10 @@
+class Sieve:
+    """Base class for all the sieves invlolved in coreference resolution"""
+    
+    def __init__(self):
+        pass
+    
+    def process(self, document):
+        """All the processing of the sieve is done here"""
+        
+        pass
